06d8cd500f540b844582a1f455974926b1a1bf5e
trisha-ravi <trisha02.r@gmail.com>
2024-06-10 11:57:00 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/website"
+  base: "/website", // Ensure that the base URL is set correctly
+  build: {
+    assetsDir: "", // Set the assets directory to the root
+  },
 })
