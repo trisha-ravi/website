479ba05188e123bee2156f26c753f5bc70b6be23
trisha-ravi <trisha02.r@gmail.com>
2024-06-09 23:50:53 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <Navbar />
       <div style={{ paddingTop: '60px' }}>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/website" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
