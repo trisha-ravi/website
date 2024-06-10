0d086b81bc424dd07cda8d3a35338e00cddb4ab0
trisha-ravi <trisha02.r@gmail.com>
2024-06-10 12:18:18 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ const App = () => {
       <Navbar />
       <div style={{ paddingTop: '60px' }}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="/website" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
